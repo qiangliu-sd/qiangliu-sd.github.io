eeab752c6b3d5b0024671856bad345d8ff9f15ce
Qiang Liu <ql12@cornell.edu>
2025-08-31 15:49:06 -0700

    Update ql_read_md.js

--- utils/ql_read_md.js
+++ utils/ql_read_md.js
@@ -2,15 +2,17 @@
 async function qlReadServerFile(url_md) {
       try {
         const response = await fetch(url_md); 
-
+		// LOAD the ql_read_md.js, which contains this function (qlReadServerFile), after markdownOut is defined
 		const htmlObj  = document.getElementById('markdownOut')
 
         if (!response.ok) throw new Error(`HTTP XCP: ${response.status}`);
         const text = await response.text();
+		  // LOAD utils/marked.min.js before calling marked.parse
 		  const htmlTxt = marked.parse(text);
-        htmlObj.textContent = htmlTxt;
+        htmlObj.innerHTML = htmlTxt;
       } catch (error) {
-        htmlObj.textContent = 'XCP fetch file: ' + error.message;
+        htmlObj.innerHTML = 'XCP fetch file: ' + error.message;
       }
 
     }
+
